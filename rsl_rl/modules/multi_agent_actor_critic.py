learning rate kl update fixed

rsl_rl/modules/multi_agent_actor_critic.py
@@ -263,7 +263,7 @@ class MultiTeamCMAAC():
 
     @property
     def entropy(self):
-        return self.teamacs[0].distribution.entropy().sum(dim=-1)
+        return self.teamacs[0].entropy
     
     @property
     def parameters(self):
@@ -313,7 +313,7 @@ class MultiTeamCMAAC():
         return values
     
     def evaluate(self, critic_observations, **kwargs):
-        value = self.teamacs[0].evaluate(critic_observations[:, self.teams[0], :])
+        value = self.teamacs[0].evaluate(critic_observations)
         return value
 
     def update_ac_ratings(self, infos):
@@ -397,6 +397,7 @@ class CMAActorCritic(nn.Module):
         self.team_size = kwargs['teamsize']
         self.action_means = []
         self.action_stds = []
+        self.action_entropies = []
 
         if self.is_attentive:
             self.ac = ActorCriticAttention(num_ego_obs=35,
@@ -452,7 +453,7 @@ class CMAActorCritic(nn.Module):
 
     @property
     def entropy(self):
-        return self.ac.actor.distribution.entropy().sum(dim=-1)
+        return torch.stack(tuple(self.action_entropies), dim = 1)
     
     #not sure how to fix these
     #@property
@@ -479,10 +480,13 @@ class CMAActorCritic(nn.Module):
         actions = []
         self.action_means = []
         self.action_stds = []
+        self.action_entropies = []
         for idx in range(self.team_size):
             actions.append(self.ac.act(observations[:, idx, :]))
             self.action_means.append(self.ac.action_mean)
             self.action_stds.append(self.ac.action_std)
+            self.action_entropies.append(self.ac.entropy)
+            
         actions = torch.stack(tuple(actions), dim = 1)
         return actions
     

rsl_rl/storage/centralized_multi_agent_rollout_storage.py
@@ -144,7 +144,7 @@ class CentralizedMultiAgentRolloutStorage:
             self.returns[step] = advantage + self.values[step]
 
         # Compute and normalize the advantages
-        self.advantages = torch.sum(self.returns - self.values, dim = (1,2))
+        self.advantages = torch.sum(self.returns - self.values, dim = (-2,-1))
         self.advantages = (self.advantages - self.advantages.mean()) / (self.advantages.std() + 1e-8)
 
     def get_statistics(self):
@@ -280,11 +280,15 @@ class CentralizedMultiAgentRolloutStorage:
                 critic_observations_batch = critic_observations[batch_idx]
                 actions_batch = actions[batch_idx]
                 target_values_batch = values[batch_idx]
+                target_values_individual_batch = target_values_batch[..., 0]
+                target_values_team_batch = target_values_batch[..., 1]
                 returns_batch = returns[batch_idx]
+                returns_individual_batch = returns_batch[..., 0]
+                returns_team_batch = returns_batch[..., 1] 
                 old_actions_log_prob_batch = old_actions_log_prob[batch_idx]
                 advantages_batch = advantages[batch_idx]
                 old_mu_batch = old_mu[batch_idx]
                 old_sigma_batch = old_sigma[batch_idx]
-                active_agents_batch = active_agents[batch_idx]
-                yield obs_batch, critic_observations_batch, actions_batch, target_values_batch, advantages_batch, returns_batch, \
-                       old_actions_log_prob_batch, old_mu_batch, old_sigma_batch, (None, None), None, active_agents_batch
+                #active_agents_batch = active_agents[batch_idx]
+                yield obs_batch, critic_observations_batch, actions_batch, target_values_individual_batch, target_values_team_batch, advantages_batch, returns_individual_batch, returns_team_batch, \
+                       old_actions_log_prob_batch, old_mu_batch, old_sigma_batch, (None, None), None, None